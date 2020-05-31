820161d03eb10cd17bb0eda76cb8137566e432b5
MarioFurrer <mariofurrer44@gmail.com>
2020-05-31 10:44:49 -0300

    upload

---
+++ pages/js/Mouse.js
@@ -0,0 +1,42 @@
+<<<<<<< HEAD
+var
+  canvas    = document.querySelector('canvas'),
+  ctx       = canvas.getContext('2d'),
+  beginPath = false;
+        
+  canvas.width  = window.innerWidth;
+  canvas.height = window.innerHeight;
+        
+  document.body.addEventListener('mousemove', function (event) {
+  var x = event.clientX, y = event.clientY;
+        
+  if (beginPath) {
+    ctx.lineTo(x, y);
+    ctx.stroke();}
+  else {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    beginPath = true;
+  }
+=======
+var
+  canvas    = document.querySelector('canvas'),
+  ctx       = canvas.getContext('2d'),
+  beginPath = false;
+        
+  canvas.width  = window.innerWidth;
+  canvas.height = window.innerHeight;
+        
+  document.body.addEventListener('mousemove', function (event) {
+  var x = event.clientX, y = event.clientY;
+        
+  if (beginPath) {
+    ctx.lineTo(x, y);
+    ctx.stroke();}
+  else {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    beginPath = true;
+  }
+>>>>>>> 51203e4d764dc3218bd1e16396238cb571be3a98
+}, false);
