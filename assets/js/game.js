849c18ac74425954d3ef6c3a20c9b6aff10578a1
MarioFurrer <mariofurrer44@gmail.com>
2020-06-08 18:55:51 -0300

    Reordenado de archivos

--- assets/js/game.js
+++ assets/js/game.js
@@ -24,12 +24,12 @@ var gameOver = false;
 var game = new Phaser.Game(config);
 
 function preload (){
-   this.load.image('sky', './assets/images/sky.png');
-   this.load.image('ground', './assets/images/platform.png');
-   this.load.image('star', './assets/images/star.png');
-   this.load.image('star2', './assets/images/star2.png');
-   this.load.image('bomb', './assets/images/bomb.png');
-   this.load.spritesheet('dude', './assets/images/dude.png',{
+   this.load.image('sky', '../assets/images/sky.png');
+   this.load.image('ground', '../assets/images/platform.png');
+   this.load.image('star', '../assets/images/star.png');
+   this.load.image('star2', '../assets/images/star2.png');
+   this.load.image('bomb', '../assets/images/bomb.png');
+   this.load.spritesheet('dude', '../assets/images/dude.png',{
        frameWidth: 32,
        frameHeight: 48
    });
